ddae825ce2bada0d19101fe3e9e4f9a70b1edb8a
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-02-24 22:22:34 +0200

    right and left buttons

--- panorama/views.py
+++ panorama/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 
 from .models import Panorama, Excursion
 def panorama(request, excursion_id=0, panorama_id=0):
-    print(excursion_id,panorama_id)
-    print(Excursion.objects.get(id=excursion_id))
-    panorama = Panorama.objects.filter(excursion=excursion_id)[panorama_id]
-    context = {'panorama': panorama}
+    #excursion_id = 1
+    panorams = Panorama.objects.filter(excursion=excursion_id)
+    num = len(panorams)
+    panorama = panorams[panorama_id]
+    context = {'panorama': panorama, "excursion_id":excursion_id, "panorama_id":panorama_id, 'num': num}
     return render(request, 'panorama/panorama.html', context);
