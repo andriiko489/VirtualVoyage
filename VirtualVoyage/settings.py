85ddf7cbeef4870855cff99873d58d6a2434e2b2
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-02-25 00:38:36 +0200

    added login and registration system, removed blue block

--- VirtualVoyage/settings.py
+++ VirtualVoyage/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'panorama.apps.PanoramaConfig',
+    'users.apps.UserConfig',
+    'social_django',
 ]
 
 MIDDLEWARE = [
