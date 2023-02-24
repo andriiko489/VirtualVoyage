a941e2bfd91b8a33e233b4d64c7609793f428d96
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-02-24 20:02:24 +0200

    templates

--- VirtualVoyage/urls.py
+++ VirtualVoyage/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from panorama.views import panorama
 urlpatterns = [
-    path('panorama/', panorama),
+    path('panorama/<int:excursion_id>/<int:panorama_id>/', panorama),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- panorama/views.py
+++ panorama/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-def index(request):
-    return HttpResponse('''''')
-def panorama(request):
-    context={}
+
+from .models import Panorama, Excursion
+def panorama(request, excursion_id=0, panorama_id=0):
+    print(excursion_id,panorama_id)
+    print(Excursion.objects.get(id=excursion_id))
+    panorama = Panorama.objects.filter(excursion=excursion_id)[panorama_id]
+    context = {'panorama': panorama}
     return render(request, 'panorama/panorama.html', context);
