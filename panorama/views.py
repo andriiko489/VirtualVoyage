2c9e9fb367774d00c11ca0f31950361ccc2c383a
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-02-25 19:20:55 +0200

    fixed bug generating template panorams.html

--- panorama/views.py
+++ panorama/views.py
@@ -2,11 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Panorama, Excursion
-def panorama(request, excursion_id=0, panorama_id=0):
+def panorama(request, excursion_id=1, panorama_id=1):
     print(excursion_id)
     panorams = Panorama.objects.filter(excursion=excursion_id)
     num = len(panorams)
-    panorama = panorams[panorama_id]
+    panorama = panorams[panorama_id-1]
     context = {'panorama': panorama, "excursion_id":excursion_id, "panorama_id":panorama_id, 'num': num}
     return render(request, 'panorama/panorama.html', context)
 def excursions(request):
